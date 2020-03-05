3d2f764e06cae69dd20cc6b477f01cbf5233391e
eversonf <eversonfgjr@gmail.com>
2020-03-05 09:26:09 -0300

    Versão Inicial

---
+++ js/questoes.js
@@ -0,0 +1,23 @@
+$(function()
+{
+    $(document).on('click', '#adicionar-criterio-filtro-btn', function(e)
+    {
+        e.preventDefault();
+
+        var controlForm = $('#collapseFiltro form:first'),
+            currentEntry = $(this).parents('.criterio-div:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('select').val('-1');
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add').addClass('btn-remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span class="fas fa-plus"></span>');
+    }).on('click', '.btn-remove', function(e)
+    {
+		$(this).parents('.entry:first').remove();
+
+		e.preventDefault();
+		return false;
+	});
+});
